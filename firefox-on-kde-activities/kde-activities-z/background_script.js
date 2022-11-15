9e41fe402175d0252cd79c9f8d4e6c63547c16a3
zdy023 <zdy004007@126.com>
2022-11-15 22:41:05 +0800

    ver Nov15th
    a prototype backup

--- firefox-on-kde-activities/kde-activities-z/background_script.js
+++ firefox-on-kde-activities/kde-activities-z/background_script.js
@@ -1,22 +1,31 @@
 // Put all the javascript code here, that you want to execute in background.
 
-var port = browser.runtime.connectNative("test_activity");
+//var port = browser.runtime.connectNative("test_activity");
 
 browser.tabs.onCreated.addListener(onTabCreated);
-port.onMessage.addListener(onMessage);
+//port.onMessage.addListener(onMessage);
 
 function onTabCreated(tab) {
-	//setTimeout( () => {
+	setTimeout( () => {
 		//browser.windows.create({tabId: tab.id});
-	//}, 3000);
-	port.postMessage( { "tabid": tab.id
-					  , "windowid": tab.windowId
-					  }
-					);
+		checkFocused(tab);
+	}, 3000);
+	//console.log("onTabCreated");
+	//let respone = browser.runtime.sendNativeMessage( "test_activity"
+									 			   //, { "tabid": tab.id
+									 			     //, "windowid": tab.windowId
+									 			     //}
+									 			   //);
+	//respone.then(onMessage);
+}
+
+async function checkFocused(tab) {
+	let windowList = await browser.windows.getAll();
+	if(windowList.every((elm) => {return !elm.focused;}))
+		browser.windows.create({tabId: tab.id});
 }
 
 function onMessage(message) {
-	setTimeout(() => {
-		browser.windows.create({tabId: message.tabid});
-	}, 3000);
+	//console.log("onMessage");
+	browser.windows.create({tabId: message.tabid});
 }
--- firefox-on-kde-activities/kde-activities-z/test_firefox_in_activities.py
+++ firefox-on-kde-activities/kde-activities-z/test_firefox_in_activities.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 
 import sys
+import json
+import struct
 
-for l in sys.stdin:
-    print(l.strip())
+message_length = sys.stdin.buffer.read(4)
+message_length = struct.unpack("=I", message_length)[0]
+message = sys.stdin.buffer.read(message_length).decode("utf-8")
+message_object = json.loads(message)
+
+response_object = message_object
+response = json.dumps(response_object).encode("utf-8")
+response_length = len(response)
+response_length = struct.pack("=I", response_length)
+sys.stdout.buffer.write(response_length)
+sys.stdout.buffer.write(response)
+sys.stdout.buffer.flush()
