d3d5634b0ee7b9f2e26407a5aace08ab499468f9
zdy023 <zdy004007@126.com>
2022-11-16 15:30:57 +0800

    ver Nov16thv4
    fixed bugs in js

--- firefox-on-kde-activities/kde-activities-z/background_script.js
+++ firefox-on-kde-activities/kde-activities-z/background_script.js
@@ -2,9 +2,7 @@
 
 var on = true;
 
-browser.tabs.onCreated.addListener((tab) => {
-	setTimeout(() => {onTabCreated(tab);}, 5000);
-});
+browser.tabs.onCreated.addListener(onTabCreated);
 browser.browserAction.onClicked.addListener(() => {
 	on = !on;
 	browser.browserAction.setIcon( { "path": { 64:
@@ -18,16 +16,22 @@ browser.browserAction.onClicked.addListener(() => {
 
 async function onTabCreated(tab) {
 	if(on) {
-		//console.log("onTabCreated");
+		console.log("onTabCreated");
 		let window_list = await browser.windows.getAll();
-		window_list = window_list.map((elm) => {
+		window_id_list = window_list.map((elm) => {
 			return elm.id;
+		});
+		window_title_list = window_list.map((elm) => {
+			return elm.title;
 		});
 		message = { "tid": tab.id
 				  , "wid": tab.windowId
-				  , "wlist": window_list
+				  , "wlist": window_id_list
+				  , "tlist": window_title_list
 				  }
-		//console.log(message.tid);
+		console.log(message.tid);
+		console.log(message.wlist);
+		console.log(message.tlist);
 		let response = await browser.runtime.sendNativeMessage( "test_activity"
 														      , message
 														      );
@@ -36,8 +40,8 @@ async function onTabCreated(tab) {
 }
 
 function onMessage(message) {
-	//console.log("onMessage");
-	//console.log(message.act);
+	console.log("onMessage");
+	console.log(message.act);
 	if(message.act=="new") {
 		browser.windows.create({tabId: message.tid});
 	}
@@ -48,7 +52,7 @@ function onMessage(message) {
 						   }
 						 )
 					.then( (tab) => {
-							browser.tabs.update( tid
+							browser.tabs.update( tab.id
 											   , { active: true
 												 }
 											   );
