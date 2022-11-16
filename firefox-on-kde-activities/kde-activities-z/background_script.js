06cc975e17f97a1033222ef6c745a48ddf6b0c2a
zdy023 <zdy004007@126.com>
2022-11-16 12:53:41 +0800

    ver Nov16th
    complete function, I think

--- firefox-on-kde-activities/kde-activities-z/background_script.js
+++ firefox-on-kde-activities/kde-activities-z/background_script.js
@@ -1,37 +1,58 @@
 // Put all the javascript code here, that you want to execute in background.
 
-//var port = browser.runtime.connectNative("test_activity");
-
 var on = true;
 
-browser.tabs.onCreated.addListener(onTabCreated);
-//port.onMessage.addListener(onMessage);
-browser.browserAction.onClicked.addListener(() => {on = false;})
+browser.tabs.onCreated.addListener((tab) => {
+	setTimeout(() => {onTabCreated(tab);}, 5000);
+});
+browser.browserAction.onClicked.addListener(() => {
+	on = !on;
+	browser.browserAction.setIcon( { "path": { 64:
+												on
+											  ? "icons/iconz.png"
+											  : "icons/icong.png"
+											 }
+								   }
+								 );
+});
 
-function onTabCreated(tab) {
-	console.log(on);
-	setTimeout( () => {
-		//browser.windows.create({tabId: tab.id});
-		checkFocused(tab);
-	}, 3000);
-	//console.log("onTabCreated");
-	//let respone = browser.runtime.sendNativeMessage( "test_activity"
-									 			   //, { "tabid": tab.id
-									 			     //, "windowid": tab.windowId
-									 			     //}
-									 			   //);
-	//respone.then(onMessage);
-}
-
-async function checkFocused(tab) {
+async function onTabCreated(tab) {
 	if(on) {
-		let windowList = await browser.windows.getAll();
-		if(windowList.every((elm) => {return !elm.focused;}))
-			browser.windows.create({tabId: tab.id});
+		//console.log("onTabCreated");
+		let window_list = await browser.windows.getAll();
+		window_list = window_list.map((elm) => {
+			return elm.id;
+		});
+		message = { "tid": tab.id
+				  , "wid": tab.windowId
+				  , "wlist": window_list
+				  }
+		//console.log(message.tid);
+		let response = await browser.runtime.sendNativeMessage( "test_activity"
+														      , message
+														      );
+		onMessage(response);
 	}
 }
 
 function onMessage(message) {
 	//console.log("onMessage");
-	browser.windows.create({tabId: message.tabid});
+	//console.log(message.act);
+	if(message.act=="new") {
+		browser.windows.create({tabId: message.tid});
+	}
+	else if(message.act=="move") {
+		browser.tabs.move( message.tid
+						 , { windowId: message.wid
+						   , index: -1
+						   }
+						 )
+					.then( (tab) => {
+							browser.tabs.update( tid
+											   , { active: true
+												 }
+											   );
+						  }
+					     );
+	}
 }
--- firefox-on-kde-activities/kde-activities-z/test_firefox_in_activities.py
+++ firefox-on-kde-activities/kde-activities-z/test_firefox_in_activities.py
@@ -3,13 +3,58 @@
 import sys
 import json
 import struct
+import subprocess
 
 message_length = sys.stdin.buffer.read(4)
 message_length = struct.unpack("=I", message_length)[0]
 message = sys.stdin.buffer.read(message_length).decode("utf-8")
 message_object = json.loads(message)
 
-response_object = message_object
+firefox_window_list = message_object["wlist"]
+firefox_window_list.sort()
+
+wmctrl = subprocess.run(["wmctrl", "-lx"], capture_output=True, text=True)
+window_list = []
+for l in wmctrl.stdout.splitlines():
+    fields = l.split()
+    if fields[2]=="Navigator.firefox":
+        window_list.append(fields[0])
+window_list.sort()
+
+current_activity = subprocess.run( [ "qdbus"
+                                   , "org.kde.ActivityManager"
+                                   , "/ActivityManager/Activities"
+                                   , "CurrentActivity"
+                                   ]
+                                 , capture_output=True
+                                 , text=True
+                                 )
+current_activity = current_activity.stdout.strip()
+#print(current_activity)
+
+target_ff_wid = None
+for wid, ff_wid in zip(window_list, firefox_window_list):
+    xprop = subprocess.run( ["xprop", "-id", wid, "_KDE_NET_WM_ACTIVITIES"]
+                          , capture_output=True
+                          , text=True
+                          )
+    activity_id = xprop.stdout.strip().split()[2][1:-1]
+    if activity_id==current_activity:
+        target_ff_wid = ff_wid
+        break
+
+if target_ff_wid is None:
+    response_object = { "act": "new"
+                      , "tid": message_object["tid"]
+                      }
+elif target_ff_wid!=message_object["wid"]:
+    response_object = { "act": "move"
+                      , "tid": message_object["tid"]
+                      , "wid": target_ff_wid
+                      }
+else:
+    response_object = {"act": "none"}
+
 response = json.dumps(response_object).encode("utf-8")
 response_length = len(response)
 response_length = struct.pack("=I", response_length)
