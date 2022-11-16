05ae4a715424e891a70636275ab82b48385be3ca
zdy023 <zdy004007@126.com>
2022-11-16 15:35:56 +0800

    ver Nov16thv5
    updated test_firefox_in_activities.py

--- firefox-on-kde-activities/test_firefox_in_activities.py
+++ firefox-on-kde-activities/test_firefox_in_activities.py
@@ -11,15 +11,16 @@ message = sys.stdin.buffer.read(message_length).decode("utf-8")
 message_object = json.loads(message)
 
 firefox_window_list = message_object["wlist"]
-firefox_window_list.sort()
+window_title_list = message_object["tlist"]
+#firefox_window_list.sort()
 
 wmctrl = subprocess.run(["wmctrl", "-lx"], capture_output=True, text=True)
 window_list = []
 for l in wmctrl.stdout.splitlines():
-    fields = l.split()
+    fields = l.split(maxsplit=4)
     if fields[2]=="Navigator.firefox":
-        window_list.append(fields[0])
-window_list.sort()
+        window_list.append((fields[0], fields[-1]))
+#window_list.sort()
 
 current_activity = subprocess.run( [ "qdbus"
                                    , "org.kde.ActivityManager"
@@ -32,28 +33,37 @@ current_activity = subprocess.run( [ "qdbus"
 current_activity = current_activity.stdout.strip()
 #print(current_activity)
 
-target_ff_wid = None
-for wid, ff_wid in zip(window_list, firefox_window_list):
+target_title = None
+for wid, ttl in window_list:
     xprop = subprocess.run( ["xprop", "-id", wid, "_KDE_NET_WM_ACTIVITIES"]
                           , capture_output=True
                           , text=True
                           )
     activity_id = xprop.stdout.strip().split()[2][1:-1]
     if activity_id==current_activity:
-        target_ff_wid = ff_wid
+        target_title = ttl
         break
 
-if target_ff_wid is None:
+if target_title is None:
     response_object = { "act": "new"
                       , "tid": message_object["tid"]
                       }
-elif target_ff_wid!=message_object["wid"]:
+else:
+    try:
+        target_ff_wid = window_title_list.index(target_title)
+        target_ff_wid = firefox_window_list[target_ff_wid]
+    except:
+        try:
+            target_ff_wid = window_title_list.index("Mozilla Firefox")
+            target_ff_wid = firefox_window_list[target_ff_wid]
+        except:
+            target_ff_wid = message_object["wid"]
+
     response_object = { "act": "move"
                       , "tid": message_object["tid"]
                       , "wid": target_ff_wid
-                      }
-else:
-    response_object = {"act": "none"}
+                      } if target_ff_wid != message_object["wid"]\
+                        else {"act": "none"}
 
 response = json.dumps(response_object).encode("utf-8")
 response_length = len(response)
